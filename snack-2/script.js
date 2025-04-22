2b53a7d3ee1a85f1fd827f7825821915b15f01f8
lucacipolloni <lucacipolloni29@gmail.com>
2025-04-22 17:29:41 +0200

    snack 2

--- snack-2/script.js
+++ snack-2/script.js
@@ -1,8 +1,13 @@
 const people = [
-  { name: 'Paolo', age: 35 },
-  { name: 'Giulia', age: 24 },
-  { name: 'Marco', age: 67 }
+  { name: "Paolo", age: 35 },
+  { name: "Giulia", age: 24 },
+  { name: "Marco", age: 67 },
 ];
 
 // Stampa in console tutti i nomi
 // Risultato: 'Paolo', 'Giulia', 'Marco'
+
+const names = people.map((person) => {
+  return person.name;
+});
+console.log(names);
